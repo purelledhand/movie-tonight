828ceb417e95443db8356a3c33e002a1d7ceb6f1
SoHye Choi <wergreat10@naver.com>
2019-01-16 23:27:52 +0900

    Make Movie Component to stateless functional  component

--- src/movie.js
+++ src/movie.js
@@ -2,20 +2,13 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './movie.css';
 
-class Movie extends Component {
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired,
-  }
-
-  render() {
-    return (
-      <div>
-        <MoviePoster poster={this.props.poster}/>
-        <h1>{this.props.title}</h1>
-      </div>
-    );
-  }
+function Movie({title, poster}) {
+  return (
+    <div>
+      <MoviePoster poster={poster}/>
+      <h1>{title}</h1>
+    </div>
+  );
 }
 
 function MoviePoster({poster}) {
@@ -23,5 +16,14 @@ function MoviePoster({poster}) {
       <img src={poster} alt="moviePoster"/>
     );
 }
+
+Movie.propTypes = {
+  title: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+};
+
+MoviePoster.propTypes = {
+  poster: PropTypes.string.isRequired,
+};
 
 export default Movie;
